22b6f7fa5f11e25396582e7d10d4a0dce93390f8
ByCarlosS <153582692+ByCarlosS@users.noreply.github.com>
2025-06-20 19:30:01 +0000

    Juego ByCarlosS

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
-//write your code here
-console.log("Hello Rigo from the console!");
-
+window.onload = function name(params) {
+    console.log("Hola usuario");
+    const Juego = ["Piedra","Papel","Tijera","Lagarto","Spock"]
+    const getRandomJuego = () => {
+        const index = Math.floor(Math.random() * Juego.length)
+        return Juego[index];
+    }
+   
+    
+    const checkJuegoGuess = (userGuess) => {
+        const randomJuego = getRandomJuego();
+        console.log(`Tú seleccionaste: ${userGuess}`);
+        console.log(`La PC selecciono: ${randomJuego}`);
+        if (userGuess === randomJuego) {
+            console.log("Empate");
+        } else if (
+            (userGuess === "Piedra" && (randomJuego === "Tijera" || randomJuego === "Lagarto")) ||
+            (userGuess === "Papel" && (randomJuego === "Piedra" || randomJuego === "Spock")) ||
+            (userGuess === "Tijera" && (randomJuego === "Papel" || randomJuego === "Lagarto")) ||
+            (userGuess === "Lagarto" && (randomJuego === "Spock" || randomJuego === "Papel")) ||
+            (userGuess === "Spock" && (randomJuego === "Tijera" || randomJuego === "Piedra"))
+        ) {
+            console.log("Ganaste");
+        } else {
+            console.log("Perdiste");
+        }
+    };
+    const userGuess = prompt("Elige: Piedra, Papel, Tijera, Lagarto o Spock");
+    checkJuegoGuess(userGuess);
+};
